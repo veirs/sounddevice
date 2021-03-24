681a2cb7d654450a06d87b2df45645facfbfb7b2
val veirs <vveirs@coloradocollege.edu>
2021-03-24 13:57:49 -0700

    Update playFile.py

--- playFile.py
+++ playFile.py
@@ -10,8 +10,9 @@ devs = sd.query_devices()
 print("here are the devices")
 print(devs)
 
-#data, fs = sf.read("PinkPanther30_a.wav", dtype = "float32")
-#print("sample rate=",fs)
-#sd.play(data, fs)
-#sd.wait()
+data, fs = sf.read("PinkPanther30_a.wav", dtype = "float32")
+print("sample rate=",fs)
+sd.play(data, fs)
+sd.wait()
 
+sudo sh get-docker.sh
